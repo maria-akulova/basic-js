ed657c37a5894d6fb7a43c7a32f43916cb57e8bb
maria.akulova <maria.akulova.dragon@gmail.com>
2023-12-24 18:58:06 +0100

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  constructor(isDirect) {
+    this.isDirect = isDirect ?? true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === null || message === undefined) throw new Error("Incorrect arguments!");
+    if (key === null || key === undefined) throw new Error("Incorrect arguments!");
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = "";
+    let symbol = "";
+
+    function getIndex(index, string) {
+      return (index > string.length) ? index % string.length : index;
+    }
+
+    if (message.length > key.length) {
+      let keyFull = key.split("");
+      let diff = message.length - key.length;
+      let times = Math.ceil(diff / key.length);
+      key = key.repeat(times + 1);
+    }
+
+    for (let i = 0; i < message.length; i++) {
+      let messageIndex = this.alphabet.indexOf(message[getIndex(i, message)]);
+      if (messageIndex === -1) {
+        result += message[i];
+        key = key.substring(0, i) + " " + key.substring(i);
+        continue;
+      }
+      let keyIndex = this.alphabet.indexOf(key[getIndex(i, key)]);
+      symbol = this.alphabet[(this.alphabet.length + messageIndex + keyIndex) % this.alphabet.length];
+
+      result += symbol;
+    }
+    return this.isDirect ? result : result.split("").reverse().join("");
+  }
+
+  decrypt(message, key) {
+    if (message === null || message === undefined) throw new Error("Incorrect arguments!");
+    if (key === null || key === undefined) throw new Error("Incorrect arguments!");
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = "";
+    let symbol = "";
+
+    function getIndex(index, string) {
+      return (index > string.length) ? index % string.length : index;
+    }
+
+    if (message.length > key.length) {
+      let keyFull = key.split("");
+      let diff = message.length - key.length;
+      let times = Math.ceil(diff / key.length);
+      key = key.repeat(times + 1);
+    }
+
+    for (let i = 0; i < message.length; i++) {
+      let messageIndex = this.alphabet.indexOf(message[getIndex(i, message)]);
+      if (messageIndex === -1) {
+        result += message[i];
+        key = key.substring(0, i) + " " + key.substring(i);
+        continue;
+      }
+      let keyIndex = this.alphabet.indexOf(key[getIndex(i, key)]);
+      symbol = this.alphabet[(this.alphabet.length + messageIndex - keyIndex) % this.alphabet.length];
+
+      result += symbol;
+    }
+    return this.isDirect ? result : result.split("").reverse().join("");
   }
 }
 
